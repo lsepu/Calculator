294ac16a6a481c6d942f87b807521e254319ff96
lsepu <55538804+lsepu@users.noreply.github.com>
2022-05-23 22:26:46 -0500

    REFACTOR: Improvement to multiplication

--- src/com/sofka/Calculator.java
+++ src/com/sofka/Calculator.java
@@ -8,12 +8,12 @@ public class Calculator {
     public static void main(String[] args) {
         ISum sum = (x,y) -> x+y;
         ISubstract substract = (x,y) -> x-y;
-        IMultiply multiply = (x,y) -> IntStream.range(0, y+1).reduce((a,b) -> sum.calculate(a,x)).getAsInt();
+        IMultiply multiply = (x,y) -> IntStream.range(0, y).reduce(0,(a,b) -> x+a);
 
 
         System.out.println("Sum example: " + sum.calculate(3,2));
         System.out.println("Substraction example: " + substract.calculate(3,2));
-        System.out.println("Multiplication example: " + multiply.calculate(2,3));
+        System.out.println("Multiplication example: " + multiply.calculate(3,2));
 
     }
 }
