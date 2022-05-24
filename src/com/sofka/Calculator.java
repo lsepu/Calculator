7ced050998cca608e4d6dd0d000a6dc1c751ec3d
lsepu <55538804+lsepu@users.noreply.github.com>
2022-05-24 00:02:12 -0500

    REFACTOR: clean the division code, add function to handle the errors

--- src/com/sofka/Calculator.java
+++ src/com/sofka/Calculator.java
@@ -1,5 +1,6 @@
 package com.sofka;
 
+import java.util.function.Function;
 import java.util.stream.IntStream;
 
 public class Calculator {
@@ -10,16 +11,17 @@ public class Calculator {
 
     private static final IMultiply multiply = (x,y) -> IntStream.range(0, y).reduce(0,(a,b) -> sum.calculate(x,a));
 
+    //using recursion to sum the iterations
+    private static final IDivide division = (x,y) -> subtract.calculate(x,y) == 0 ? 1 : Calculator.division.calculate(subtract.calculate(x,y), y) + 1;
+
     /**
-     * using recursion to count the iterations.
-     * @return -1 if 0 is the divisor
+     * @return Error if 0 is the divisor
      * @return 0 if 0 is the dividend
      * @return the result of the division if it has the appropriate values
      */
-    private static final IDivide division = (x,y) ->
-            y == 0 ? -1 :
-                    x == 0 ? 0 :
-                            subtract.calculate(x,y) == 0 ? 1 : Calculator.division.calculate(subtract.calculate(x,y), y) + 1;
+    private static String divide(Integer x, Integer y) {
+        return y == 0 ? "Error" : x == 0 ? "0" : division.calculate(x,y).toString();
+    }
 
     public static void main(String[] args) {
 
@@ -27,13 +29,13 @@ public class Calculator {
         System.out.println("Subtraction example: " + subtract.calculate(10,2));
         System.out.println("Multiplication example: " + multiply.calculate(10,2));
 
-        Integer divisionByZero = division.calculate(2,0);
-
-        System.out.println("Division example: " + division.calculate(24,2));
-        System.out.println("Division example with 0 as dividend: " + division.calculate(0,2));
-        System.out.println("Division example with 0 as divisor: " + (divisionByZero == -1 ? "Error" : divisionByZero));
+        System.out.println("Division example: " + divide(24,2));
+        System.out.println("Division example with 0 as dividend: " + divide(0,2));
+        System.out.println("Division example with 0 as divisor: " + divide(2,0));
 
     }
+
+
 
 
 }
